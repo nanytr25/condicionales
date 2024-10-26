ce5a50f57f4fbf2ac3209f02a99341d5c615d0bc
Nancy Guadalupe Robles Trejo <nanytr259@gmail.com>
2024-10-26 17:02:48 -0600

    Ejercicio 3

---
+++ ejercicio3.py
@@ -0,0 +1,10 @@
+"""
+Escribe un programa que lea un número e indique si es par o impar.
+"""
+print("programa que verifica si un numero es par o impar")
+num = int(input("Ingresa un numero: "))
+if num % 2 == 0:
+    print("El numero es par")
+else:
+    print("El numero es inpar")    
+print("Fin del programa") 
