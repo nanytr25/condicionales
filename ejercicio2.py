15229819a38329d1e2b8676836dbe93873d99f71
Nancy Guadalupe Robles Trejo <nanytr259@gmail.com>
2024-10-26 17:01:02 -0600

    Ejercicio 2

--- ejercicio2.py
+++ ejercicio2.py
@@ -3,7 +3,6 @@ Programa que pida un número y diga si es positivo, negativo o 0.
 """
 print("Programa que verifica si un numero es positivo, negativo o 0")
 num = int(input("Ingresa un numero: "))
-# preguntar el numero es mayor a 0:
 if num == 0:
     print("El numero es 0")
 else:
