3238225f5bd046770098973c7a1cb0301b26ff78
Nancy Guadalupe Robles Trejo <nanytr259@gmail.com>
2024-11-03 10:47:37 -0600

    Ejercicio 6

---
+++ ejercicio6.py
@@ -0,0 +1,10 @@
+"""
+ Programa que lea una cadena por teclado y compruebe si es una letra mayúscula.
+"""
+user = input("Ingresa tu nombre de usuario:")
+passw = input("Ingresa la contraseña:")
+if user == "Nany" and passw == "nanytr" :
+    print("Has ingresado al sistema!")
+else: 
+    print("Usuario o Contraseña incorrectos!")
+print("Fin del programa")
